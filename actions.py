ff42d718e15d153b3cf4f3d203cfa6526c2a7613
Balaj Khalid <balaj.khalid@ymail.com>
2020-09-13 22:42:10 +0500

    Added support for other facilities and expanded search to all cities

--- actions.py
+++ actions.py
@@ -13,6 +13,8 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
 
+key = "enter-your-api-key" # enter the API shared in group
+
 class ActionFindFacility(Action):
 
     def name(self) -> Text:
@@ -22,23 +24,28 @@ class ActionFindFacility(Action):
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
         loc = tracker.get_slot('location') #obtain location
-        loc = loc.lower() # to make it easier to compare
 
-        location = ""
+        # to get lattitude and longitude of the areas
+        URL = "https://maps.googleapis.com/maps/api/geocode/json"
 
-        # coded for 3 major cities as need longitude and latitude for search via API
-        if loc == "karachi":
-            location = "24.8607,67.0011"
-        elif loc == "islamabad":
-            location = "33.6844,73.0479"
-        elif loc == "lahore":
-            location = "31.5204, 74.3587"
+        PARAMS = {'address': loc,  'key': key} 
+  
+        # sending get request and saving the response as response object 
+        r = requests.get(url = URL, params=PARAMS) 
+        
+        # extracting data in json format 
+        data = r.json() 
+
+        lat = data['results'][0]['geometry']['location']['lat']
+        long = data['results'][0]['geometry']['location']['lng']
+        location = str(lat) + "," + str(long)
+
+        # to find the nearby facility
+        URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
         
         radius = "1500"
-        type = "hospital"
-        key = "enter-your-api" # enter the API shared in group
+        type = tracker.get_slot('facility_type')
         
         PARAMS = {'location':location, 'radius' : radius, 'opennow' : True, 'type': type, 'key': key} 
         
@@ -47,15 +54,12 @@ class ActionFindFacility(Action):
         
         # extracting data in json format 
         data = r.json() 
-        names = []
+        final_resp = "" #format the final response in name, vicinity format
         i = 0
         for x in data:
-            names.append(data['results'][i]['name'])
+            final_resp += "Name: " + data['results'][i]['name'] + " Vicinity: " +  data['results'][i]['vicinity'] + "\n"
             i += 1
-
-        # using list comprehension 
-        listToStr = ' '.join(map(str, names)) 
  
-        dispatcher.utter_message(text = listToStr)
+        dispatcher.utter_message(text = final_resp)
         
         return []
