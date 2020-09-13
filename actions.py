0431233e614f72337d3f739193163562a5aeb831
Balaj Khalid <balaj.khalid@ymail.com>
2020-09-13 13:38:54 +0500

    Updated actions.py to search for hospitals

--- actions.py
+++ actions.py
@@ -11,7 +11,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
+import requests
 
 class ActionFindFacility(Action):
 
@@ -19,9 +19,43 @@ class ActionFindFacility(Action):
         return "action_find_facility"
 
     def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Balaj hasn't programmed me to find facilities yet lolz")
+        URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+        loc = tracker.get_slot('location') #obtain location
+        loc = loc.lower() # to make it easier to compare
 
+        location = ""
+
+        # coded for 3 major cities as need longitude and latitude for search via API
+        if loc == "karachi":
+            location = "24.8607,67.0011"
+        elif loc == "islamabad":
+            location = "33.6844,73.0479"
+        elif loc == "lahore":
+            location = "31.5204, 74.3587"
+        
+        radius = "1500"
+        type = "hospital"
+        key = "enter-your-api" # enter the API shared in group
+        
+        PARAMS = {'location':location, 'radius' : radius, 'opennow' : True, 'type': type, 'key': key} 
+        
+        # sending get request and saving the response as response object 
+        r = requests.get(url = URL, params=PARAMS) 
+        
+        # extracting data in json format 
+        data = r.json() 
+        names = []
+        i = 0
+        for x in data:
+            names.append(data['results'][i]['name'])
+            i += 1
+
+        # using list comprehension 
+        listToStr = ' '.join(map(str, names)) 
+ 
+        dispatcher.utter_message(text = listToStr)
+        
         return []
